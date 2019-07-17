f0feef906ab460a26810e03c616d43580a8a7d84
Jonathan del Strother <jdelStrother@gmail.com>
2019-07-17 11:02:13 +0100

    Add fetch-mock

--- src/stories/index.js
+++ src/stories/index.js
@@ -6,6 +6,8 @@ import { linkTo } from '@storybook/addon-links';
 
 import { Button, Welcome } from '@storybook/react/demo';
 
+import fetchMock from "fetch-mock"
+
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
 storiesOf('Button', module)
